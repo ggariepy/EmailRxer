0c6436a219db42c6422375037d3a897f8a5cbf3d
ggariepy <geoff.gariepy@gmail.com>
2018-12-30 21:55:45 -0500

    Modified to use Python 3.7 style classes

--- EmailRxer.py
+++ EmailRxer.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python3
 #  EmailRxer.py
+""" Polls the email account configured in configs.json
+    and retrieves new messages, decodes them into their
+    component parts, converts them to instances of the
+    Message class, and places them into a FIFO queue.
+    Typically deletes messages after retrieving them;
+    this behavior can be changed by configuring the
+    deleteMsgs item in configs.json to false.   
+"""
 import poplib as pop
 import json as js
 import MessageClasses as mclass
@@ -14,16 +22,22 @@ popPass = configs['popPass']
 popServer = configs['popServer']
 popPort = configs['popPort']
 pollFreq = configs['pollFreq']
+deleteMsgs = configs['deleteMsgs']
 
+# Establish connection to POP3 server
 M = pop.POP3(popServer)
 M.user(popUser)
 M.pass_(popPass)
-messageBody = False
+
+# If there are messages, decode them from 
+# byte arrays and retrieve the headers we
+# want and their message bodies
 Messages = {}
-headers = {}
-Body = ''
 numMessages = len(M.list()[1])
 for messagenum in range(numMessages):
+    headers = {}
+    Body = ''
+    messageBody = False
     for line in M.retr(messagenum+1)[1]:
         if messageBody == False:
             segments = line.split(b': ')
@@ -39,7 +53,7 @@ for messagenum in range(numMessages):
                     headers[key] = val
         else: Body += line.decode('ASCII')
     Messages[messagenum] = (headers, Body)
-    #M.dele(messagenum + 1)        
+    deleteMsgs and M.dele(messagenum + 1)        
 M.quit()
 
 print ("Retrieved " + str(numMessages) + " message(s).")
@@ -55,4 +69,4 @@ for msg in Messages.items():
     thismsg = mclass.Message(strTo, strFrom, strDate, strSubject, strBody)
     msgQueue.put(thismsg)
 
-print ("Enqueued " + str(msgQueue._qsize()) + " messages")
+print ("Enqueued " + str(msgQueue._qsize()) + " message(s)")
--- MessageClasses.py
+++ MessageClasses.py
@@ -1,17 +1,27 @@
 import time
+from dataclasses import dataclass, field
 
+@dataclass(order=True)
 class Message:
-    """Defines a class which represents a single email message 
-    retrieved from a POP server."""
-    
-    def __init__(self, _To, _From, _Date, _Subject, _Body):
-       self.To = _To
-       self.From = _From
-       self.DateString = _Date
-       self.Subject = _Subject
-       self.Body = _Body 
-       self.DateConv()
- 
-    def DateConv(self):
-        # Sat, 29 Dec 2018 17:05:23 -0500
+    """
+    Defines a class which represents a single email message 
+    retrieved from a POP server.  
+    Fields:
+    To <str>
+    From <str> 
+    DateString <str>, e.g. Fri, 31 Dec 1999 23:59:59 -0500
+    Subject 
+    Body
+    """
+    To: str
+    From: str
+    Subject: str
+    Body: str
+    DateString: str = field(default='Fri, 31 Dec 1999 23:59:59 -0500')
+
+    def __post_init__(self):
+        """ 
+        Creates the TimeStamp attribute by
+        converting the text DateString to a time value
+        """
         self.TimeStamp = time.strptime(self.DateString, "%a, %d %b %Y %H:%M:%S %z") 
